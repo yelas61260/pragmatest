51b391a46d07603608e6b6d67c0316c3d571d832
Cristian Yela <cristian.yela@pragma.com.co>
2022-06-15 08:14:47 -0500

    first commit

---
+++ imagemanager/src/main/java/com/pragma/imagemanager/ImagemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.imagemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagemanagerApplication.class, args);
+	}
+
+}
---
+++ imagemanager/src/test/java/com/pragma/imagemanager/ImagemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.imagemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
